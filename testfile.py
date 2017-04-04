ef06ef032b51ce6ea771d58bd69a4c1518d9ec7c
Oneironaut21 <mblinow@gmail.com>
2017-04-04 18:40:04 -0400

    Finished game data lookup function.  Started on user input search

--- testfile.py
+++ testfile.py
@@ -6,16 +6,43 @@ from bs4 import BeautifulSoup
 ##Creating cookie to bypass age gate
 cookies = {'birthtime': '568022401', 'mature_content': '1'}
 
-#Getting target page html
-res = requests.get('http://store.steampowered.com/app/608800/', cookies=cookies)
-res.raise_for_status()
+def printGameInfo(gameIDNum):
+    requestString='http://store.steampowered.com/app/'+str(gameIDNum)+'/'
+    #res = requests.get('http://store.steampowered.com/app/608800/', cookies=cookies)
+    #Getting target page html
+    res = requests.get(requestString, cookies=cookies)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, 'html.parser')
+    gamePrice=soup.find(class_="price")
+    if gamePrice==None:
+        gamePrice=soup.find(class_="discount_final_price").string
+        gamePrice=gamePrice+' on sale'
+    else: gamePrice=gamePrice.string
+    gameName=soup.find(class_="apphub_AppName").string
+    print (gameName.encode('utf8'))
+    #print (gameName)
+    print (str(gamePrice).strip())
 
-soup = BeautifulSoup(res.text, 'html.parser')
+def formatUserInput(userInput):
+    formattedInput=''
+    for i in range(len(userInput)):
+        if userInput[i]==' ':
+            formattedInput+='+'
+        else:
+            formattedInput+=userInput[i]
+    return formattedInput
+
+def gameSearch():
+    #userInput=input('Enter Game Name: ')
+    userInput='Quake 2'
+    formattedInput=formatUserInput(userInput)
 
 
-gamePrice=soup.find(class_="price").string
-gameName=soup.find(class_="apphub_AppName").string
+    requestString='http://store.steampowered.com/search/?snr=1_4_4__12&term='+formattedInput
+    res = requests.get(requestString)
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-
-
-print (str(gamePrice).strip())
+#printGameInfo(608800)
+#printGameInfo(374320)
+#printGameInfo(403640)
+gameSearch()
